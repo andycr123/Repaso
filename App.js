4742a0757894be9fe96b3107b8bb94d00e21f254
andycr123 <81699558+andycr123@users.noreply.github.com>
2024-04-02 16:48:53 -0500

    Finish

--- App.js
+++ App.js
@@ -1,11 +1,36 @@
-/*Tipos de datos */
-//Boleanos, Null, Undefined, String, symbol, Number, y object
+/* 
+Ejercicio #1: Documentgetelementbyid: 
+  // Obtener el elemento por su ID
+  var tituloElemento = document.getElementById("título");
+  // Cambiar el contenido del elemento
+  tituloElemento.innerHTML = "Nuevo Título";
+*/
 
-var  a; 
-var b = 2; 
+/* 
+Ejercicio #2 = getElementsByTagName()
+  // Obtener todos los elementos 'li' dentro del 'ul'
+  var listaItems = document.getElementById("lista").getElementsByTagName("li");
+  // Iterar sobre los elementos y cambiar su contenido
+  for (var i = 0; i < listaItems.length; i++) {
+    listaItems[i].innerHTML = "Nuevo Elemento " + (i+1);
+  }
+*/
 
-consle.log(a); /* Undefinded     */
+/*
+Ejercicio #3 = querySelector():
+  // Obtener el primer elemento 'p' dentro del div con clase 'contenedor'
+  var primerParrafo = document.querySelector(".contenedor p");
+  // Cambiar el contenido del párrafo
+  primerParrafo.innerHTML = "Nuevo Texto 1";
+*/
 
-a = 10; 
+/* 
+Ejercicio #4=  querySelectorAll():
+// Obtener todos los elementos 'p' dentro del div con clase 'contenedor'
+  var parrafos = document.querySelectorAll(".contenedor p");
+  // Iterar sobre los elementos y cambiar su contenido
+  parrafos.forEach(function(parrafo, index) {
+    parrafo.innerHTML = "Nuevo Texto " + (index + 1);
+  });
 
-console.log(a)
+*/
